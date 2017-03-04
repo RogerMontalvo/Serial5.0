2dd1188cd49ff1c6cfdb9ce913534b4be0af7e41
RogerMontalvo <rogermontalvo7@gmail.com>
2017-03-04 16:39:09 +0100

    1.0

--- _5.6/_5.6.ino
+++ _5.6/_5.6.ino
@@ -6,57 +6,29 @@
 ** NOM: Nom de l'Alumne                               DATA: xx/xx/20xx **
 ************************************************************************/
 //**************************** INCLUDE **********************************
-#include <stdio.h>
+
 
 //*************************** VARIABLES *********************************
 
-int Numero ;
-int Control ;
-
+int Numero[15];
+int i;
 //**************************** SETUP ************************************
 
 
 void setup()             // CONDICIONS INICIALS
 {                           
  Serial.begin (9600) ;
- Serial.println ("Int valor per saber si es enter o senar: ") ;
- 
-
+ Serial.print ("Int numeros per acabar enter per saber la mitja") ;
 
 }
 
 //**************************** LOOP *************************************
 
-void loop()                        // BUCLE
-{                           
- while (Serial.available()  >0) 
- 
-{
-
- Numero= Serial.parseInt () ;
- if ( Serial.read()== '\n') // Preguntem si s'ha acabat de introduir el numero (press enter)
- {
-
-  
- Control= Numero %2 ;
- 
- Serial.print ("-El numero ") ; 
- Serial.print (Numero) ;
- if(Control ==0)
-{ 
- 
- Serial.println (" es Parell ") ;
+void loop()             // BUCLE
+{                   
+  for (i = 0; i<=15; i++){        
+ Numero [i]= Serial.parseInt() ;
+  }
 }
-else
-{
- Serial.println (" es Senar") ;
-}
- Serial.println ("") ;
 
- Serial.println ("Int valor per saber si es enter: ") ;
-
-
-}
-}
-}
 //*************************** FUNCIONS **********************************
