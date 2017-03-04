1e8d9e1984927fdc7042f8c44cd05604bb3af85d
RogerMontalvo <rogermontalvo7@gmail.com>
2017-03-04 12:37:25 +0100

    Final5.1

--- _5.1/_5.1.ino
+++ _5.1/_5.1.ino
@@ -19,6 +19,7 @@ void setup()                  // CONDICIONS INICIALS
 {                           
   Serial.begin(9600);        // initialize serial
   Serial.println("Entra el valor de r1 i r2 (separats per una coma)");
+  Serial.println ("") ;
 }
 
 
@@ -30,12 +31,38 @@ void loop()                  // BUCLE
 {
   while (Serial.available() > 0) {  // if there's any serial available, read it
     r1 = Serial.parseInt();// look for valid int the incoming serial stream
-    CODI ALUMNE (mostra el valor de r1)
+Serial.print ("El valor de r1 es: ");
+Serial.println ( r1) ;
+ Serial.print ("El valor de r2 es: ");   
     r2 = Serial.parseInt();
-    CODI ALUMNE (mostra el valor de r2)
+   
+    Serial.println ( r2) ;
+    Serial.println ("") ;
+
     if (Serial.read() == '\n') { //look for newline. Is the end of your sentence
-    CODI ALUMNE (calcula r1 i r2 en serie i paral·lel, mostra resultats i                      
-                 fa de nou la pregunta inicial)
+      
+//    CODI ALUMNE (calcula r1 i r2 en serie i paral·lel, mostra resultats i                      
+               //  fa de nou la pregunta inicial)
+
+rSerie= r1+r2 ;
+
+rParalel= (r1*r2)/(r1+r2) ;
+
+Serial.print ( "Les resistencies en serie valen: ") ;
+//Serial.print (" ");   -També per ficar espai
+Serial.println ( rSerie) ;
+
+Serial.print ( "Les resistencies en paralel valen: ") ;
+Serial.println ( rParalel) ;
+Serial.println ("") ;
+
+Serial.println("Entra el valor de r1 i r2 (separats per una coma)");
+Serial.println ("") ;
+
+
+
+
+               
     }
   }
 }                            
